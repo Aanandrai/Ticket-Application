375f16a7b3272a8b4006198fd41e95537a2713b0
Aanand rai <aanandrai.atwork@gmail.com>
2025-04-20 10:50:40 +0530

    push 2

--- app/src/main/java/org/ticketBooking/services/UserBookingService.java
+++ app/src/main/java/org/ticketBooking/services/UserBookingService.java
@@ -1,20 +1,31 @@
 package org.ticketBooking.services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.ticketBooking.entities.User;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class UserBookingService {
 
 
     private User user;
 
+    private List<User> userList;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
     private static final String USER_PATH="../localDb/user.json";
 
-    public UserBookingService(User user1){
+    public UserBookingService(User user1) throws IOException
+    {
         this.user=user1;
 
         File users= new File(USER_PATH);
+
+        userList=objectMapper.readValue(users , new TypeReference<List<User>>(){});
     }
 
 }
