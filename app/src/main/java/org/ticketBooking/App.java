e9c7db9d9b02441f8b1c561ca192fd95ee8c997b
Aanand rai <aanandrai.atwork@gmail.com>
2025-05-01 22:42:12 +0530

    working on app file

--- app/src/main/java/org/ticketBooking/App.java
+++ app/src/main/java/org/ticketBooking/App.java
@@ -9,9 +9,7 @@ import org.ticketBooking.services.UserBookingService;
 import org.ticketBooking.util.UserServiceUtil;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.UUID;
+import java.util.*;
 
 public class App {
     public String getGreeting() {
@@ -61,7 +59,70 @@ public class App {
                     break;
 
                 case 2:
+                    System.out.println("Enter the username to Login");
+                    String nameToLogin = scanner.next();
+                    System.out.println("Enter the password to signup");
+                    String passwordToLogin = scanner.next();
+                    User userToLogin = new User(nameToLogin , passwordToLogin, UserServiceUtil.hashPassword(passwordToLogin), new ArrayList<>(),UUID.randomUUID().toString());
+                    try{
+                        userBookingService =new UserBookingService(userToLogin);
+
+                    }catch(Exception e){
+                        return;
+                    }
                     break;
+
+                case 3:
+                    System.out.println("Featch your booking");
+                    userBookingService.fetchBookings();
+                    break;
+
+                case 4:
+                    System.out.println("Type your source station");
+                    String source = scanner.next();
+                    System.out.println("Type your destination station");
+                    String dest = scanner.next();
+                    List<Train> trains =userBookingService.getTrains(source , dest);
+                    int index =1;
+
+                    for(Train t:trains){
+                        System.out.println(index+" Train id : "+t.getTrainId());
+                        for( Map.Entry<String, String> entry : t.getStationTimes().entrySet()){
+                            System.out.println("station "+entry.getKey()+" time: "+entry.getValue());
+                        }
+                    }
+                    System.out.println("Select a train by type 1,2,3...");
+                    trainSelectedForBooking=trains.get(scanner.nextInt());
+                    break;
+
+                case 5:
+                    System.out.println("Select a seat out of these seats");
+                    List<List<Integer>>seats = userBookingService.fetchSeats(trainSelectedForBooking);
+                    for(List<Integer> row:seats){
+                        for(Integer val:row){
+                            System.out.print(val+" ");
+                        }
+                        System.out.println();
+
+                    }
+
+                    System.out.println("Select the seat by typing the row and column");
+                    System.out.println("Enter the row");
+                    int row = scanner.nextInt();
+                    System.out.println("Enter the column");
+                    int col=scanner.nextInt();
+                    System.out.println("Booking your seat ...");
+                    Boolean booked = userBookingService.bookTrainSeat(trainSelectedForBooking, row , col);
+                    if(booked.equals(Boolean.TRUE)){
+                        System.out.println("Booked! Enjoy your journey");
+                    }else{
+                        System.out.println("Can't book this seat");
+                    }
+                    break;
+
+                default:
+                    break;
+
             }
         }
 
